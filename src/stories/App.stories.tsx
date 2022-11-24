59c618e371452d308cc7fe37204462d2fab7e5d8
Sebastian <sebastian.besel@nethesis.it>
2022-11-24 18:06:45 +0100

    Get config string from env in stories

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -3,11 +3,11 @@
 
 import { Story, Meta } from '@storybook/react'
 import React from 'react'
-import { App } from '../App'
+import { PhoneIsland } from '../App'
 
 const meta = {
   title: 'App Widget',
-  component: App,
+  component: PhoneIsland,
   argTypes: {},
   parameters: {
     controls: { expanded: true },
@@ -16,21 +16,10 @@ const meta = {
 
 export default meta as Meta
 
-// On user login
-// /me
-// /login // Token no expire
+// Uses the configuration token from .env
+const config = process.env.CONFIG_TOKEN
 
-const HOST_NAME: string = 'nv-seb'
-const USERNAME: string = 'foo1'
-const AUTH_TOKEN: string = '791ff10b8666939426eb1b5507e983558f0e5806'
-const SIP_EXTEN: string = '211'
-const SIP_SECRET: string = '0081a9189671e8c3d1ad8b025f92403da'
-
-const config = btoa(HOST_NAME + ':' + USERNAME + ':' + AUTH_TOKEN + ':' + SIP_EXTEN  + ':' + SIP_SECRET)
-
-console.log(config)
-
-const Template: Story<any> = (args) => <App dataConfig={config} {...args} />
+const Template: Story<any> = (args) => <PhoneIsland dataConfig={config} {...args} />
 
 export const Default = Template.bind({})
 Default.args = {}
