480e8985b0b9b4367e01406d9f91278fbc26e05d
Sebastian <sebastian.besel@nethesis.it>
2023-01-27 09:33:43 +0100

    Remove scale effect

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -16,7 +16,7 @@ function classNames(...classes) {
 
 export const Button: FC<ButtonProps> = ({ children, variant, active = false, ...props }) => {
   const styles = {
-    base: 'flex content-center items-center justify-center font-medium tracking-wide duration-200 transform focus:outline-none focus:ring-2 focus:z-20 focus:ring-offset-2 disabled:opacity-75 text-white border border-transparent focus:ring-offset-black rounded-full text-sm leading-4 h-12 w-12 col-start-auto transition duration-200 ease-in-out',
+    base: 'flex content-center items-center justify-center font-medium tracking-wide duration-200 transform focus:outline-none focus:ring-2 focus:z-20 focus:ring-offset-2 disabled:opacity-75 text-white border border-transparent focus:ring-offset-black rounded-full text-sm leading-4 h-12 w-12 col-start-auto transition-color',
     variant: {
       red: 'bg-red-600 hover:bg-red-700 focus:ring-red-500',
       green: 'bg-green-600 hover:bg-green-700 focus:ring-green-500',
@@ -25,7 +25,7 @@ export const Button: FC<ButtonProps> = ({ children, variant, active = false, ...
         'bg-transparent hover:bg-gray-500 hover:border-gray-500 border border-gray-700 focus:ring-0',
     },
     active: {
-      default: 'scale-110 bg-gray-500',
+      default: 'bg-gray-500',
     },
   }
 
