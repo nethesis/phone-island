9fa1b38b0d7b53adfaca8ec161109afd9766fa9c
Sebastian <sebastian.besel@nethesis.it>
2023-04-06 18:55:50 +0200

    Split Island and improve motions maintainability

---
+++ src/components/IslandDrag.tsx
@@ -0,0 +1,123 @@
+// Copyright (C) 2022 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import React, { type ReactNode, FC, useState, useRef, MutableRefObject } from 'react'
+import { RootState, Dispatch } from '../store'
+import { useSelector, useDispatch } from 'react-redux'
+import { motion, useDragControls } from 'framer-motion/dist/framer-motion'
+import { useLongPress, useLocalStorage, styleTransformValues } from '../utils'
+import { xPosition, yPosition } from '../lib/island/island'
+
+export const IslandDrag: FC<IslandDragProps> = ({ children, islandContainerRef }) => {
+  const { startPosition } = useSelector((state: RootState) => state.island)
+
+  // Initialize the moved property
+  const [moved, setMoved] = useState<boolean>(false)
+
+  // Initialize dispatch
+  const dispatch = useDispatch<Dispatch>()
+
+  // Initialize Island drag controls
+  const controls = useDragControls()
+
+  // Initialize Island storage
+  const [phoneIslandStorage, setPhoneIslandStorage] =
+    useLocalStorage<PhoneIslandStorageTypes | null>('phone-island', null)
+
+  // The Island reference
+  const islandRef = useRef<any>(null)
+
+  // Initialize position or get from storage
+  const [position, setPosition] = useState<PositionTypes | null>(
+    phoneIslandStorage && phoneIslandStorage.position ? phoneIslandStorage.position : null,
+  )
+
+  // Handles the drag started event
+  function handleStartDrag(event) {
+    controls.start(event)
+  }
+  // Handles log press event
+  const handleLongPress = () => {}
+
+  // Handle Island click
+  const handleIslandClick = () => {
+    dispatch.island.toggleIsOpen()
+  }
+
+  // Handles drag end event
+  const handleDragEnd = () => {
+    // Get initial transform values
+    let { x, y }: any = styleTransformValues(islandRef.current)
+    // Round position
+    x = xPosition(Math.round(x), islandRef.current, islandContainerRef.current)
+    y = yPosition(Math.round(y), islandRef.current, islandContainerRef.current)
+    // Save the new position to localstorage
+    setPhoneIslandStorage({
+      position: {
+        x,
+        y,
+      },
+    })
+    // Set position to variable
+    setPosition({
+      x,
+      y,
+    })
+  }
+
+  // Handles drag started event
+  function handleDragStarted() {
+    setMoved(true)
+  }
+
+  // Initialize the longPressEvent object
+  const longPressEvent = useLongPress(
+    handleLongPress,
+    handleIslandClick,
+    moved,
+    () => setMoved(false),
+    {
+      shouldPreventDefault: true,
+      delay: 250,
+    },
+  )
+
+  return (
+    <motion.div
+      drag
+      onPointerDown={handleStartDrag}
+      onDragStart={handleDragStarted}
+      dragTransition={{
+        power: 0,
+      }}
+      initial={{
+        x: position?.x || startPosition.x,
+        y: position?.y || startPosition.y,
+      }}
+      dragControls={controls}
+      dragConstraints={islandContainerRef}
+      onDragEnd={handleDragEnd}
+      ref={islandRef}
+      {...longPressEvent}
+      className='pi-absolute'
+    >
+      {children && children}
+    </motion.div>
+  )
+}
+
+export default IslandDrag
+
+export interface IslandDragProps {
+  children: ReactNode
+  islandContainerRef: MutableRefObject<HTMLDivElement>
+}
+
+interface PhoneIslandStorageTypes {
+  position: PositionTypes
+}
+
+interface PositionTypes {
+  x: number
+  y: number
+}
---
+++ src/components/IslandMotion.tsx
@@ -0,0 +1,121 @@
+// Copyright (C) 2022 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import React, { type ReactNode, FC } from 'react'
+import { RootState } from '../store'
+import { useSelector } from 'react-redux'
+import { motion } from 'framer-motion/dist/framer-motion'
+
+export const IslandMotion: FC<IslandMotionProps> = ({ children }) => {
+  // Retrieve needed stored variables
+  const { incoming, outgoing, accepted, transferring } = useSelector(
+    (state: RootState) => state.currentCall,
+  )
+  const { view, isOpen } = useSelector((state: RootState) => state.island)
+  const { activeAlertsCount } = useSelector((state: RootState) => state.alerts.status)
+  const {
+    variants,
+    border_radius_collapsed,
+    border_radius_expanded,
+    padding_x_collapsed,
+    padding_y_collapsed,
+    padding_expanded,
+  } = useSelector((state: RootState) => state.motions)
+
+  function getVariant() {
+    // Initial size
+    let size: SizeTypes = {
+      width: 0,
+      height: 0,
+    }
+    switch (view) {
+      case 'call':
+        if (isOpen) {
+          if (accepted && transferring) {
+            size = {
+              width: variants.call.expanded.transfer.width,
+              height: variants.call.expanded.transfer.height,
+            }
+          } else if (accepted) {
+            size = {
+              width: variants.call.expanded.accepted.width,
+              height: variants.call.expanded.accepted.height,
+            }
+          } else if (incoming) {
+            size = {
+              width: variants.call.expanded.incoming.width,
+              height: variants.call.expanded.incoming.height,
+            }
+          } else if (outgoing) {
+            size = {
+              width: variants.call.expanded.outgoing.width,
+              height: variants.call.expanded.outgoing.height,
+            }
+          }
+        } else {
+          size = {
+            width: variants.call.collapsed.width,
+            height: variants.call.collapsed.height,
+          }
+        }
+        break
+      case 'keypad':
+        if (isOpen) {
+          size = {
+            width: variants.keypad.expanded.width,
+            height: variants.keypad.expanded.height,
+          }
+        } else {
+          size = {
+            width: variants.transfer.collapsed.width,
+            height: variants.transfer.collapsed.height,
+          }
+        }
+        break
+      case 'transfer':
+        if (isOpen) {
+          size = {
+            width: variants.transfer.expanded.width,
+            height: variants.transfer.expanded.height,
+          }
+        } else {
+          size = {
+            width: variants.transfer.collapsed.width,
+            height: variants.transfer.collapsed.height,
+          }
+        }
+        break
+    }
+
+    return {
+      width: `${size.width}px`,
+      height: `${activeAlertsCount > 0 ? size.height + variants.alerts.height : size.height}px`,
+      borderRadius: isOpen ? `${border_radius_expanded}px` : `${border_radius_collapsed}px`,
+      padding: isOpen
+        ? `${padding_expanded}px`
+        : `${padding_x_collapsed}px ${padding_y_collapsed}px`,
+    }
+  }
+
+  const motionVariants = getVariant()
+
+  return (
+    <motion.div
+      className='pi-font-sans pi-pointer-events-auto pi-overflow-hidden pi-bg-black pi-text-xs pi-cursor-pointer pi-text-white'
+      animate={motionVariants}
+    >
+      {children && children}
+    </motion.div>
+  )
+}
+
+export interface IslandMotionProps {
+  children: ReactNode
+}
+
+type SizeTypes = {
+  width: number
+  height: number
+}
+
+export default IslandMotion
