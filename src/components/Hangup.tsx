6ff8edd42786482bb3babcbee09389b5af3484aa
Sebastian <sebastian.besel@nethesis.it>
2023-05-04 14:53:03 +0200

    Add success message on attended transfer

--- src/components/Hangup.tsx
+++ src/components/Hangup.tsx
@@ -7,27 +7,41 @@ import { Button } from './Button'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPhone } from '@nethesis/nethesis-solid-svg-icons'
 import { hangupCurrentCall } from '../lib/phone/call'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../store'
+import { Dispatch } from '../store'
 
 /**
  * Return the status of the
  */
 const Hangup: FC<HangupProps> = ({ clickCallback, isDestination, description }) => {
-  const { view } = useSelector((state: RootState) => state.island)
   const { transferring } = useSelector((state: RootState) => state.currentCall)
+  const dispatch = useDispatch<Dispatch>()
+
+  function handleHangup() {
+    hangupCurrentCall()
+    // Show confirmation message when a call is transferred
+    if (transferring) {
+      setTimeout(() => {
+        dispatch.alerts.setAlert('call_transfered')
+        setTimeout(() => {
+          dispatch.alerts.removeAlert('call_transfered')
+        }, 2000)
+      }, 300)
+    }
+  }
 
   return (
     <div className={`pi-flex pi-justify-center ${transferring && 'pi-w-full'}`}>
       {/* The button to hangup the currentCall */}
       <motion.div className={`${transferring && description ? 'pi-w-full' : 'pi-w-12'}`}>
         <Button
-          onClick={() => hangupCurrentCall()}
+          onClick={() => handleHangup()}
           variant='red'
           className='pi-gap-4 pi-font-medium pi-text-base pi-transition pi-min-w-12 pi-w-full'
         >
           <FontAwesomeIcon className='pi-rotate-135 pi-h-6 pi-w-6' icon={faPhone} />
-          {(transferring && description) && (
+          {transferring && description && (
             <motion.div
               style={{ height: '17px' }}
               className='pi-whitespace-nowrap pi-overflow-hidden'
