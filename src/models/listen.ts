843b6f6404bbb97075dfc8dee1a1414ccbcd8f29
Antonio <36625268+tonyco97@users.noreply.github.com>
2023-10-23 11:34:22 +0200

    Changed UI for listen and intrude (#34)

---
+++ src/models/listen.ts
@@ -0,0 +1,38 @@
+// Copyright (C) 2022 Nethesis S.r.l.
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+import { createModel } from '@rematch/core'
+import type { RootModel } from '.'
+
+interface DefaultState {
+  isListen: boolean
+  isListenExtension: string
+  isIntrude: boolean
+  isIntrudeExtension: string
+}
+
+const defaultState: DefaultState = {
+  isListen: false,
+  isListenExtension: '',
+  isIntrude: false,
+  isIntrudeExtension: '',
+}
+
+export const listen = createModel<RootModel>()({
+  state: defaultState,
+  reducers: {
+    setUpdateListenStatus: (state, isListen: boolean, isListenExtension: string) => {
+      state.isListen = isListen
+      state.isListenExtension = isListenExtension
+      return state
+    },
+    setUpdateIntrudeStatus: (state, isIntrude: boolean, isIntrudeExtension: string) => {
+      state.isIntrude = isIntrude
+      state.isIntrudeExtension = isIntrudeExtension
+      return state
+    },
+    reset: () => {
+      return defaultState
+    },
+  },
+})
