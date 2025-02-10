ebad2c40fa0a9d9160e7972cba6c3b3f44841657
tonyco97 <tonyco97.tc@gmail.com>
2025-02-10 14:17:26 +0100

    fix: wrong storybook translations

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,39 @@
-// Copyright (C) 2024 Nethesis S.r.l.
+// Copyright (C) 2025 Nethesis S.r.l.
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 import '../src/index.css'
+import i18next from 'i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import { initReactI18next } from 'react-i18next'
+
+import translationIT from '../public/locales/it/translation.json'
+import translationEN from '../public/locales/en/translation.json'
+
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      it: {
+        translation: translationIT,
+      },
+      en: {
+        translation: translationEN,
+      },
+    },
+    lng: 'it',
+    fallbackLng: ['en'],
+    load: 'languageOnly',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['navigator'],
+    },
+  })
 
 export const parameters = {
   layout: 'fullscreen',
